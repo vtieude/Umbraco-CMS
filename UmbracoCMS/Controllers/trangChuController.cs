1951bd31f59eecc3c14b79c81bab6a8262a01afe
Wilson <wilson@enclave.vn>
2017-12-20 16:20:05 +0700

    Creat basic Homepage with Umbraco CMS

---
+++ UmbracoCMS/Controllers/trangChuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+namespace UmbracoCMS.Controllers
+{
+    public class trangChuController : RenderMvcController
+    {
+        //Get: perSon
+        public ActionResult TrangChu(RenderModel model)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                var email = Request("email");
+                var contentService = Services.ContentService;
+                var newperson = contentService.CreateContent(email,model.Content.Id, "person");
+                newperson.SetValue("email",email);
+                contentService.SaveAndPublishWithStatus(newperson);
+                Console.WriteLine("muahahaah");
+            }
+             Console.WriteLine("muahahaah");
+            return CurrentTemplate(model);
+        }
+    }
+}
